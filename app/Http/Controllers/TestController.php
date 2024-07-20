be604c3dab633a609a2e672817ee7d4250561ae6
Mohamed Husain <mWX1248776@china.huawei.com>
2024-07-20 20:57:18 +0300

    first push

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+
+// ??give me function to return test sentance
+public function test(){
+    return view("test");
+    }
+}
